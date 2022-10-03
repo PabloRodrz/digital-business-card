12bf10e3708da511571ed09535f814c3772ac790
PabloRodrz <pablorodrz09@gmail.com>
2022-10-02 23:03:48 -0300

    Technologies added

--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -4,8 +4,8 @@ function Interests() {
   return (
     <div className='interests-container'>
       <h3>Skills</h3>
-      <p>Html. Css. Javascript. React.js. Redux. <br />
-      Soft skills: Empathy, good listener and communicator, willingness to learn, commitment, responsibility, 
+      <p>Javascript. React.js. Redux. Router. Bootstrap. HTML5. CSS. <br />
+      <b>Soft skills: </b>  Empathy, good listener and communicator, willingness to learn, commitment, responsibility, 
       growth mindset, creativity and flexibility
       </p>
     </div>
